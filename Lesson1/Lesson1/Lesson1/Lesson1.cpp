f527e1001c77ffc85e54aff475dc8d99df776048
Anastasia <morugina.anast@mail.ru>
2023-05-27 21:59:21 +0300

    Commit1

---
+++ Lesson1/Lesson1/Lesson1/Lesson1.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ifstream input("in.txt");
+    ofstream output("out.txt");
+
+    int n, m;
+    input >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        input >> a[i];
+    }
+
+    input >> m;
+    vector<int> b(m);
+    for (int i = 0; i < m; i++) {
+        input >> b[i];
+    }
+
+    reverse(a.begin(), a.end());
+    output << m << endl;
+    for (int i = 0; i < m; i++) {
+        output << b[i] << " ";
+    }
+    output << endl;
+
+    reverse(b.begin(), b.end());
+    output << n << endl;
+    for (int i = 1; i < n; i++) {
+        output << a[i] << " ";
+    }
+    output << a[0] << endl;
+
+    return 0;
+}
