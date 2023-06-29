a79abb589235c3fea5d282c3cf7b46c5309c3d54
Anastasia <morugina.anast@mail.ru>
2023-06-29 14:16:37 +0300

    Lesson3 Done

---
+++ Lesson3/Zadanie 1/Zadanie 1.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+
+class Calculator {
+private:
+    double num1;
+    double num2;
+
+public:
+    double add() {
+        return num1 + num2;
+    }
+
+    double multiply() {
+        return num1 * num2;
+    }
+
+    double subtract_1_2() {
+        return num1 - num2;
+    }
+
+    double subtract_2_1() {
+        return num2 - num1;
+    }
+
+    double divide_1_2() {
+        return num1 / num2;
+    }
+
+    double divide_2_1() {
+        return num2 / num1;
+    }
+
+    bool set_num1(double num1) {
+        if (num1 != 0) {
+            this->num1 = num1;
+            return true;
+        }
+        return false;
+    }
+
+    bool set_num2(double num2) {
+        if (num2 != 0) {
+            this->num2 = num2;
+            return true;
+        }
+        return false;
+    }
+};
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    Calculator calculator;
+    double num1, num2;
+
+    while (true) {
+        std::cout << "Введите num1: ";
+        std::cin >> num1;
+        std::cout << "Введите num2: ";
+        std::cin >> num2;
+
+        if (calculator.set_num1(num1) && calculator.set_num2(num2)) {
+            std::cout << "num1 + num2 = " << calculator.add() << std::endl;
+            std::cout << "num1 - num2 = " << calculator.subtract_1_2() << std::endl;
+            std::cout << "num2 - num1 = " << calculator.subtract_2_1() << std::endl;
+            std::cout << "num1 * num2 = " << calculator.multiply() << std::endl;
+            std::cout << "num1 / num2 = " << calculator.divide_1_2() << std::endl;
+            std::cout << "num2 / num1 = " << calculator.divide_2_1() << std::endl;
+        }
+        else {
+            std::cout << "Неверный ввод!" << std::endl;
+        }
+
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
---
+++ Lesson3/Zadanie 2/Zadanie 2.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+
+class Counter {
+private:
+    int value;
+
+public:
+    Counter() : value(1) {}
+
+    Counter(int initialValue) : value(initialValue) {}
+
+    void increment() {
+        value++;
+    }
+
+    void decrement() {
+        value--;
+    }
+
+    int getValue() const {
+        return value;
+    }
+};
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    char command;
+    std::string initialize;
+    int initialValue;
+
+    std::cout << "Вы хотите указать начальное значение счётчика? Введите да или нет: ";
+    std::cin >> initialize;
+
+    Counter counter;
+
+    if (initialize == "да") {
+        std::cout << "Введите начальное значение счётчика: ";
+        std::cin >> initialValue;
+        counter = Counter(initialValue);
+    }
+
+    do {
+        std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
+        std::cin >> command;
+
+        switch (command) {
+        case '+':
+            counter.increment();
+            break;
+        case '-':
+            counter.decrement();
+            break;
+        case '=':
+            std::cout << counter.getValue() << std::endl;
+            break;
+        case 'x':
+            std::cout << "До свидания!" << std::endl;
+            return 0;
+        default:
+            std::cout << "Неверная команда!" << std::endl;
+            break;
+        }
+    } while (true);
+
+    return 0;
+}
+
+
+
+
