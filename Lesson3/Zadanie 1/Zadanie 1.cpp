decd70b5e241f77b353f925d67c5fffbbe81b0cb
Anastasia <morugina.anast@mail.ru>
2023-07-02 20:53:19 +0300

    Zadanie 1 Lesson 3 new

--- Lesson3/Zadanie 1/Zadanie 1.cpp
+++ Lesson3/Zadanie 1/Zadanie 1.cpp
@@ -55,23 +55,29 @@ int main() {
     while (true) {
         std::cout << "Введите num1: ";
         std::cin >> num1;
+
+        if (calculator.set_num1(num1)) {
+            break;
+        }
+        std::cout << "Неверный ввод!" << std::endl;
+    }
+
+    while (true) {
         std::cout << "Введите num2: ";
         std::cin >> num2;
 
-        if (calculator.set_num1(num1) && calculator.set_num2(num2)) {
-            std::cout << "num1 + num2 = " << calculator.add() << std::endl;
-            std::cout << "num1 - num2 = " << calculator.subtract_1_2() << std::endl;
-            std::cout << "num2 - num1 = " << calculator.subtract_2_1() << std::endl;
-            std::cout << "num1 * num2 = " << calculator.multiply() << std::endl;
-            std::cout << "num1 / num2 = " << calculator.divide_1_2() << std::endl;
-            std::cout << "num2 / num1 = " << calculator.divide_2_1() << std::endl;
+        if (calculator.set_num2(num2)) {
+            break;
         }
-        else {
-            std::cout << "Неверный ввод!" << std::endl;
-        }
-
-        std::cout << std::endl;
+        std::cout << "Неверный ввод!" << std::endl;
     }
+
+    std::cout << "num1 + num2 = " << calculator.add() << std::endl;
+    std::cout << "num1 - num2 = " << calculator.subtract_1_2() << std::endl;
+    std::cout << "num2 - num1 = " << calculator.subtract_2_1() << std::endl;
+    std::cout << "num1 * num2 = " << calculator.multiply() << std::endl;
+    std::cout << "num1 / num2 = " << calculator.divide_1_2() << std::endl;
+    std::cout << "num2 / num1 = " << calculator.divide_2_1() << std::endl;
 
     return 0;
 }
